82d288d70885ca5ec329c8a0a1047f1aff21e213
Lara Sanders <larasanders@gmail.com>
2018-01-19 20:31:47 +0000

    Done.

---
+++ app/controllers/tweets_contoller.rb
@@ -0,0 +1,69 @@
+class UsersController < ApplicationController
+
+get '/tweets/new' do
+  if session[:user_id]
+    erb :'tweets/new'
+  else
+    redirect to '/login'
+  end
+end
+
+get '/tweets/:id' do
+  @tweet = Tweet.find_by_id(session[:user_id])
+    if session[:user_id] = @user.id
+  erb :'tweets/show'
+else
+  redirect to '/login'
+end
+end
+
+get '/tweets/:id/edit' do
+  @tweet = Tweet.find_by_id(params[:id])
+     if @tweet.user_id == session[:user_id]
+  erb :'tweets/edit'
+else
+  redirect to '/login'
+
+end
+end
+
+
+
+
+
+post '/tweets/:id' do
+  if params[:content] == nil
+    redirect to "/tweets/#{@tweet.id}/edit"
+  else
+  @tweet = Tweet.find_by_id(params[:id])
+  @tweet.content = Content.create(params[:content])
+  @tweet.update(params[:tweet])
+  @tweet.save
+  redirect to "/tweets/#{@tweet.id}"
+end
+end
+
+post '/tweets' do
+if params[:content] == nil
+  redirect to "tweets/new"
+else
+  user = User.find_by_id(session[:user_id])
+  @tweet = Tweet.create(:content => params[:content], :user_id => user.id)
+  redirect to "/tweets/#{@tweet.id}"
+end
+end
+
+
+post 'tweets/:id/delete' do
+    if session[:user_id]
+      @tweet = Tweet.find_by_id(params[:id])
+       if @tweet.user_id == session[:user_id]
+         @tweet.delete
+         redirect to '/tweets'
+       else
+         redirect to '/tweets'
+       end
+     else
+       redirect to '/login'
+     end
+end
