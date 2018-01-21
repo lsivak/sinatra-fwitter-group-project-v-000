239e5fbda5740fa6bd65df534bc4a6775b2226af
Lara Sanders <larasanders@gmail.com>
2018-01-21 21:55:46 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,14 @@ get '/' do
   erb :index
 end
 
+helpers do
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    User.find(session[:user_id])
+  end
+
+end
 end
--- app/controllers/tweets_contoller.rb
+++ app/controllers/tweets_contoller.rb
@@ -1,9 +1,4 @@
 class TweetsController < ApplicationController
-  #
-  # get '/' do
-  #   @tweets = Tweet.all
-  #   erb :'/index'
-  # end
 
 get '/tweets' do
   if session[:user_id]
@@ -22,9 +17,20 @@ get '/tweets/new' do
   end
 end
 
+post '/tweets' do
+if @user.logged_in? && params[:content] == ""
+  redirect to "tweets/new"
+else
+  user = User.find_by_id(session[:user_id])
+  @tweet = Tweet.create(:content => params[:content], :user_id => user.id)
+  @tweet.save
+  redirect to "/tweets/#{@tweet.id}"
+end
+end
+
 get '/tweets/:id' do
-  @tweet = Tweet.find_by_id(params[:id])
     if session[:user_id]
+  @tweet = Tweet.find_by_id(params[:id])
   erb :'tweets/show'
 else
   redirect to '/login'
@@ -32,17 +38,17 @@ end
 end
 
 get '/tweets/:id/edit' do
-  @tweet = Tweet.find_by_id(params[:id])
-     if @tweet.user_id == session[:user_id]
+  if session[:user_id]
+  # @tweet = Tweet.find_by_id(params[:id])
+  #    if @tweet.user_id == session[:user_id]
   erb :'tweets/edit'
-else
-  redirect to '/tweets'
+end
+if !session[:user_id]
+  redirect to '/login'
+end
 end
 
-end
-end
-
-post '/tweets/:id' do
+patch '/tweets/:id' do
   if params[:content] == nil
     redirect to "/tweets/#{@tweet.id}/edit"
   else
@@ -53,17 +59,6 @@ post '/tweets/:id' do
   redirect to "/tweets/#{@tweet.id}"
 end
 end
-
-post '/tweets' do
-if params[:content] == ""
-  redirect to "tweets/new"
-else
-  user = User.find_by_id(session[:user_id])
-  @tweet = Tweet.create(:content => params[:content], :user_id => user.id)
-  redirect to "/tweets/#{@tweet.id}"
-end
-end
-
 
 delete 'tweets/:id/delete' do
       @tweet = Tweet.find_by_id(params[:id])
