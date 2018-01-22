8899de55a2bebdc4805e3964acd704898163b140
Lara Sanders <larasanders@gmail.com>
2018-01-22 22:31:14 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
    if !session[:user_id]
      erb :'users/signup'
    else
+
      redirect '/tweets'
  end
 end
@@ -22,11 +23,13 @@ end
   post '/signup' do
     # if params[:username] =="" || params[:password] ==""|| params[:email] ==""
     if params[:username].empty? || params[:password].empty? || params[:email].empty?
-      redirect to '/signup'
+      redirect '/signup'
+
     else
-   @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+   @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
    @user.save
    session[:user_id] = @user.id
+
    redirect '/tweets'
  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
     username.downcase.gsub(" ","-" )
   end
 
-  def find_by_slug(slug)
-      User.all.find{ |user| user.slug == slug }
+  def self.find_by_slug(slug)
+      self.all.find{ |user| user.slug == slug }
     end
   end
